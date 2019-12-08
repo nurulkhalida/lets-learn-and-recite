43bd9572f5bd1255e264ebf554c1e87fc70e9079
Dony Tamara <dony.tamara@photobookworldwide.com>
2019-12-08 20:27:17 +0800

    bikin kelas Huruf buat nampung data

---
+++ app/src/main/java/com/casa/letslearnandrecite/learn/hijaiyah/Huruf.java
@@ -0,0 +1,30 @@
+package com.casa.letslearnandrecite.learn.hijaiyah;
+
+public class Huruf {
+    private String nama;
+    private int gambar;
+
+    public Huruf() {
+    }
+
+    public Huruf(String nama, int gambar) {
+        this.nama = nama;
+        this.gambar = gambar;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getGambar() {
+        return gambar;
+    }
+
+    public void setGambar(int gambar) {
+        this.gambar = gambar;
+    }
+}
