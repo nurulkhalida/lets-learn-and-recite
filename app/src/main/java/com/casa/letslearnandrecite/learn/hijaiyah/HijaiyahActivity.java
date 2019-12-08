7fd1cd3bf43a6ba7b02c8c852f1b01f15877b97b
sypaindoman <sypaindoman@gmail.com>
2019-12-08 21:11:53 +0800

    Isi data hurup hijaiyah

--- app/src/main/java/com/casa/letslearnandrecite/learn/hijaiyah/HijaiyahActivity.java
+++ app/src/main/java/com/casa/letslearnandrecite/learn/hijaiyah/HijaiyahActivity.java
@@ -49,11 +49,34 @@ public class HijaiyahActivity extends AppCompatActivity implements HurufAdapter.
 
     public void isiDataHuruf() {
         //Tambah data ke RecyclerView
-        hurufList.add(new Huruf("Ha", R.drawable.ha));
-        hurufList.add(new Huruf("Ta", R.drawable.ta));
+        hurufList.add(new Huruf("Alif", R.drawable.alif));
         hurufList.add(new Huruf("Ba", R.drawable.ba));
+        hurufList.add(new Huruf("Ta", R.drawable.ta));
         hurufList.add(new Huruf("Sa", R.drawable.sa));
         hurufList.add(new Huruf("Ja", R.drawable.ja));
+        hurufList.add(new Huruf("Ha", R.drawable.ha));
+        hurufList.add(new Huruf("Kho", R.drawable.kho));
+        hurufList.add(new Huruf("Da", R.drawable.da));
+        hurufList.add(new Huruf("Dza", R.drawable.dza));
+        hurufList.add(new Huruf("Ro", R.drawable.ro));
+        hurufList.add(new Huruf("Dzal", R.drawable.dzal));
+        hurufList.add(new Huruf("Sya", R.drawable.sya));
+        hurufList.add(new Huruf("Syin", R.drawable.syin));
+        hurufList.add(new Huruf("So", R.drawable.so));
+        hurufList.add(new Huruf("Dot", R.drawable.dot));
+        hurufList.add(new Huruf("To", R.drawable.to));
+        hurufList.add(new Huruf("Zo", R.drawable.zo));
+        hurufList.add(new Huruf("Ain", R.drawable.ain));
+        hurufList.add(new Huruf("Goin", R.drawable.goin));
+        hurufList.add(new Huruf("Fa", R.drawable.fa));
+        hurufList.add(new Huruf("Qof", R.drawable.qof));
+        hurufList.add(new Huruf("Kal", R.drawable.kal));
+        hurufList.add(new Huruf("Lam", R.drawable.lam));
+        hurufList.add(new Huruf("Mim", R.drawable.mim));
+        hurufList.add(new Huruf("Nun", R.drawable.nun));
+        hurufList.add(new Huruf("Wau", R.drawable.wau));
+        hurufList.add(new Huruf("Haha", R.drawable.haha));
+        hurufList.add(new Huruf("Ya", R.drawable.ya));
     }
 
     public void settingRecyclerView() {
