bf4875579eb6e912e09271a794db78b7df524aef
sypaindoman <sypaindoman@gmail.com>
2019-12-12 01:04:14 +0800

    setting quiz

--- app/src/main/java/com/casa/letslearnandrecite/quiz/adapter/Pilihan.java
+++ app/src/main/java/com/casa/letslearnandrecite/quiz/adapter/Pilihan.java
@@ -1,4 +1,27 @@
 package com.casa.letslearnandrecite.quiz.adapter;
 
 public class Pilihan {
+    private String pilih;
+    private String jawab;
+
+    public Pilihan(String pilih, String jawab) {
+        this.pilih = pilih;
+        this.jawab = jawab;
+    }
+
+    public String getPilih() {
+        return pilih;
+    }
+
+    public void setPilih(String pilih) {
+        this.pilih = pilih;
+    }
+
+    public String getJawab() {
+        return jawab;
+    }
+
+    public void setJawab(String jawab) {
+        this.jawab = jawab;
+    }
 }
--- app/src/main/java/com/casa/letslearnandrecite/quiz/adapter/PilihanAdapter.java
+++ app/src/main/java/com/casa/letslearnandrecite/quiz/adapter/PilihanAdapter.java
@@ -1,4 +1,62 @@
 package com.casa.letslearnandrecite.quiz.adapter;
 
-public class PilihanAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.casa.letslearnandrecite.R;
+
+import java.util.List;
+
+public class PilihanAdapter extends RecyclerView.Adapter<PilihanAdapter.PilihanViewHolder> {
+
+    public interface PilihanClickListener {
+        void onPilihanClicked(Pilihan pilihan);
+    }
+
+    private List<Pilihan> pilihanList;
+    private PilihanAdapter.PilihanClickListener clickListener;
+
+    public class PilihanViewHolder extends RecyclerView.ViewHolder {
+        public TextView pilihanTextView;
+
+        public PilihanViewHolder(View view) {
+            super(view);
+            pilihanTextView = view.findViewById(R.id.bg_quiz_option);
+        }
+    }
+
+
+    public PilihanAdapter(List<Pilihan> pilihanList, PilihanAdapter.PilihanClickListener clickListener) {
+        this.pilihanList = pilihanList;
+        this.clickListener = clickListener;
+    }
+
+    @Override
+    public PilihanViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_quiz_pilihan, parent, false);
+
+        return new PilihanViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(PilihanViewHolder holder, int position) {
+        final Pilihan pilihan = pilihanList.get(position);
+        holder.pilihanTextView.setText(pilihan.getPilih());
+        holder.pilihanTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickListener.onPilihanClicked(pilihan);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return pilihanList.size();
+    }
 }
