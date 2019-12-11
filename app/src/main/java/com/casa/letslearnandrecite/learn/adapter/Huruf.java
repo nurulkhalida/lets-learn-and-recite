b3044cd8a8e2b3d10d986ca90ead4724763c6b98
sypaindoman <sypaindoman@gmail.com>
2019-12-11 18:55:39 +0800

    nambahin suara ke tiap huruf hijaiyah

--- app/src/main/java/com/casa/letslearnandrecite/learn/adapter/Huruf.java
+++ app/src/main/java/com/casa/letslearnandrecite/learn/adapter/Huruf.java
@@ -3,13 +3,21 @@ package com.casa.letslearnandrecite.learn.adapter;
 public class Huruf {
     private String nama;
     private int gambar;
+    private int suara;
 
     public Huruf() {
+    }
+
+    public Huruf(String nama, int gambar, int suara) {
+        this.nama = nama;
+        this.gambar = gambar;
+        this.suara = suara;
     }
 
     public Huruf(String nama, int gambar) {
         this.nama = nama;
         this.gambar = gambar;
+        this.suara = -1;
     }
 
     public String getNama() {
@@ -26,5 +34,13 @@ public class Huruf {
 
     public void setGambar(int gambar) {
         this.gambar = gambar;
+    }
+
+    public int getSuara() {
+        return suara;
+    }
+
+    public void setSuara(int suara) {
+        this.suara = suara;
     }
 }
--- app/src/main/java/com/casa/letslearnandrecite/learn/hijaiyah/HijaiyahActivity.java
+++ app/src/main/java/com/casa/letslearnandrecite/learn/hijaiyah/HijaiyahActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -21,6 +23,9 @@ public class HijaiyahActivity extends AppCompatActivity implements HurufAdapter.
     private RecyclerView hurufRecyclerView;
     private HurufAdapter hurufAdapter;
     private ImageView hurufImageView;
+
+    //Bikin Media Player kalo mau mainin musik
+    MediaPlayer mpSuara;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,38 +50,48 @@ public class HijaiyahActivity extends AppCompatActivity implements HurufAdapter.
     public void onHurufClicked(Huruf huruf) {
         //Ganti gambar hurufnya setiap di click
         hurufImageView.setImageResource(huruf.getGambar());
+
+        //Kalo ada suara yang play di hapus suara yang sebelumnya
+        if (mpSuara != null && mpSuara.isPlaying()){
+            //hapus suara sebelumnya
+            mpSuara.release();
+        }
+        //masukin suara baru
+        mpSuara = MediaPlayer.create(this, huruf.getSuara());
+        //play suara
+        mpSuara.start();
     }
 
     public void isiDataHuruf() {
         //Tambah data ke RecyclerView
-        hurufList.add(new Huruf("Alif", R.drawable.alif));
-        hurufList.add(new Huruf("Ba", R.drawable.ba));
-        hurufList.add(new Huruf("Ta", R.drawable.ta));
-        hurufList.add(new Huruf("Sa", R.drawable.sa));
-        hurufList.add(new Huruf("Ja", R.drawable.ja));
-        hurufList.add(new Huruf("Ha", R.drawable.ha));
-        hurufList.add(new Huruf("Kho", R.drawable.kho));
-        hurufList.add(new Huruf("Da", R.drawable.da));
-        hurufList.add(new Huruf("Dza", R.drawable.dza));
-        hurufList.add(new Huruf("Ro", R.drawable.ro));
-        hurufList.add(new Huruf("Dzal", R.drawable.dzal));
-        hurufList.add(new Huruf("Sya", R.drawable.sya));
-        hurufList.add(new Huruf("Syin", R.drawable.syin));
-        hurufList.add(new Huruf("So", R.drawable.so));
-        hurufList.add(new Huruf("Dot", R.drawable.dot));
-        hurufList.add(new Huruf("To", R.drawable.to));
-        hurufList.add(new Huruf("Zo", R.drawable.zo));
-        hurufList.add(new Huruf("Ain", R.drawable.ain));
-        hurufList.add(new Huruf("Goin", R.drawable.goin));
-        hurufList.add(new Huruf("Fa", R.drawable.fa));
-        hurufList.add(new Huruf("Qof", R.drawable.qof));
-        hurufList.add(new Huruf("Kal", R.drawable.kal));
-        hurufList.add(new Huruf("Lam", R.drawable.lam));
-        hurufList.add(new Huruf("Mim", R.drawable.mim));
-        hurufList.add(new Huruf("Nun", R.drawable.nun));
-        hurufList.add(new Huruf("Wau", R.drawable.wau));
-        hurufList.add(new Huruf("Haha", R.drawable.haha));
-        hurufList.add(new Huruf("Ya", R.drawable.ya));
+        hurufList.add(new Huruf("Alif", R.drawable.alif, R.raw.alif));
+        hurufList.add(new Huruf("Ba", R.drawable.ba, R.raw.ba));
+        hurufList.add(new Huruf("Ta", R.drawable.ta, R.raw.ta));
+        hurufList.add(new Huruf("Sa", R.drawable.sa, R.raw.tsa));
+        hurufList.add(new Huruf("Ja", R.drawable.ja, R.raw.ja));
+        hurufList.add(new Huruf("Ha", R.drawable.ha, R.raw.ha));
+        hurufList.add(new Huruf("Kho", R.drawable.kho, R.raw.kho));
+        hurufList.add(new Huruf("Da", R.drawable.da, R.raw.da));
+        hurufList.add(new Huruf("Dza", R.drawable.dza, R.raw.dza));
+        hurufList.add(new Huruf("Ro", R.drawable.ro, R.raw.ro));
+        hurufList.add(new Huruf("Dzal", R.drawable.dzal, R.raw.dzal));
+        hurufList.add(new Huruf("Sya", R.drawable.sya, R.raw.sin));
+        hurufList.add(new Huruf("Syin", R.drawable.syin, R.raw.syin));
+        hurufList.add(new Huruf("So", R.drawable.so, R.raw.so));
+        hurufList.add(new Huruf("Dot", R.drawable.dot, R.raw.do_huruf));
+        hurufList.add(new Huruf("To", R.drawable.to, R.raw.to));
+        hurufList.add(new Huruf("Zo", R.drawable.zo, R.raw.tzo));
+        hurufList.add(new Huruf("Ain", R.drawable.ain, R.raw.ain));
+        hurufList.add(new Huruf("Goin", R.drawable.goin, R.raw.ain));
+        hurufList.add(new Huruf("Fa", R.drawable.fa, R.raw.fa));
+        hurufList.add(new Huruf("Qof", R.drawable.qof, R.raw.qof));
+        hurufList.add(new Huruf("Kal", R.drawable.kal, R.raw.kal));
+        hurufList.add(new Huruf("Lam", R.drawable.lam, R.raw.lam));
+        hurufList.add(new Huruf("Mim", R.drawable.mim, R.raw.mim));
+        hurufList.add(new Huruf("Nun", R.drawable.nun, R.raw.nun));
+        hurufList.add(new Huruf("Wau", R.drawable.wau, R.raw.wau));
+        hurufList.add(new Huruf("Haha", R.drawable.haha, R.raw.hamzah));
+        hurufList.add(new Huruf("Ya", R.drawable.ya, R.raw.ya));
     }
 
     public void settingRecyclerView() {
